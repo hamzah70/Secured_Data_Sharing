43fef66cf40a80f6cc5329d53e6dabb37ebc6c44
himanshuraj18 <himanshu18038@iiitd.ac.in>
2020-11-04 13:45:05 +0530

    updated

--- didStructure.py
+++ didStructure.py
@@ -5,6 +5,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.asymmetric import padding
+from temp import column_sets
 
 class did:
 
