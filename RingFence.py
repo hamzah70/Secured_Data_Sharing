e1645dcd08878ce578c69fdf204779f2abef50c2
Anuneet Anand <anuneet18022@iiitd.ac.in>
2020-11-14 14:56:36 +0530

    Modified Policy Loading

--- RingFence.py
+++ RingFence.py
@@ -1,14 +1,118 @@
 # Secured Data Movement
 # Ring Fence
 
+import json
 import uuid
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives.asymmetric import padding
-from clustering import Hospital_Clusters
+from cryptography.hazmat.primitives.asymmetric import rsa,padding
+
+class rid:
+
+    '''
+        Record Identifier
+            - Uniquely identifies a user record
+            - Loads the relevant policy
+            - Segregate attributes according to policy into segments
+            - Generates key pairs for different segments
+    '''
+
+    __uniqueID = None
+    __policy = None
+    __privateKey = {}           
+    __publicKey = {}
+    __shared_data = {}
+    __confidential_data = []      
+
+    def __init__(self, policy_document):
+
+        self.uniqueID = __gen_ID()
+
+        with open(policy_document,"r") as file:
+            self.__policy = json.load(file)
+
+        file.close()
+
+        __update()
+
+    def __gen_ID(self):
+        # Generates a unique identifier and append it to the record of generated identifiers.
+        
+        RID = set()
+        
+        with open("RID.txt","r") as file:
+            data = file.readlines()
+            for i in data : 
+                RID.append(i)
+
+        file.close()
+
+        x = uuid.uuid1()
+        while x in RID:
+            x = uuid.uuid1()
+
+        RID.add(x)
+
+        with open("RID.txt","a") as file:
+            file.write(str(x)+"\n")
+
+        file.close()
+
+        self.__uniqueID = x
+
+    def __update():
+        # Resolves shareable data and generate keys
+
+        for ring in self.__policy:
+            self.__shared_data[ring] = []
+            for attribute in self.__policy[ring]:
+                if ring[attribute] == 1:
+                    self.__shared_data[ring].append(attribute)
+                else:
+                    self.__confidential_data.append(attribute)
+
+        public, private = self.__generateKeys()
+
+        self.public_key["Secret"] = public
+        self.private_key["Secret"] = None
+
+        for ring in self.__shared_data:
+            public, private = self.__generateKeys()
+            self.public_key[ring] = public
+            self.private_key[ring] = private
+
+    def setPolicy(self, custom_policy):
+        with open(custom_policy,"r") as file:
+            self.__policy = json.load(file)
+        __update()
+
+    def getPolicy(self):
+        return self.__policy
+
+    def getID(self):
+        return self.__uniqueID
+
+    def getSharedData(self):
+        return self.__shared_data
+
+    def getPublicKey(self):
+        return self.__publicKey
+
+    def __generateKeys(self):
+        private_key = rsa.generate_private_key( public_exponent=65537, key_size=2048, backend=default_backend())
+        public_key = private_key.public_key()
+        return public_key, private_key
+
 
 class ring_fence:
+
+    '''
+        Ring Fenced Data Block
+            - Accesses data using RID
+            - Creates the ring fences and enforces encryption
+            - Dissolves the ring fences
+            - Returns Extracted Data
+    '''
 
     __RID = None
     Data_Block = {}
@@ -17,21 +121,29 @@ class ring_fence:
         self.__RID = rid
 
     def create(self, *args):
-        for ring in self.__RID.getPolicy()["health_records"].keys():
+        # Creates the ring fence based on policy agreement.
+
+        for ring in self.__RID.__shared_data:
             self.Data_Block[i] = {} 
 
         for label, data in args:
-            for ring in self.__RID.getPolicy()["health_records"].keys():
-                if label in self.__RID.getPolicy()["health_records"][ring]:
+            for ring in self.__RID.getSharedData():
+                if label in self.__RID.getSharedData()[ring]:
                     key = self.__RID.getPublicKey()[ring]
                     encryptedData = encryptData(data,key)
                     Data_Block[ring][label] = encryptData
                 else:
                     Data_Block[ring][label] = None
 
+        Data_Block["MetaData"] = {}
+        Data_Block["MetaData"]["RID"] = self.__RID.getID()
+        Data_Block["MetaData"]["Policy"] = self.__RID.getPolicy()
+
         return Data_Block
 
-    def break_(self, keys):
+    def dissolve(self, keys):
+        # Dissolves the ring fence based on policy agreement.
+
         Decrypted_Data = {}
 
         for ring in self.Data_Block.keys():
@@ -67,85 +179,4 @@ class ring_fence:
         return decryptedData
 
 
-class rid:
 
-    __uniqueID = None
-    __policy = None
-    __privateKey = {}			
-    __publicKey = {}
-    __shared_data = {}
-    __confidential_data = []
-    __dataAccess = {}  		
-
-    def __init__(self, policy):
-        for d in (policy["health_records"]["Medical_Records_1"], policy["health_records"]["Medical_Records_2"], 
-            policy["health_records"]["Medical_Records_3"], policy["health_records"]["Addictions"]): self.__dataAccess.update(d)
-        self.uniqueID = __gen_ID()
-        self.__policy = policy
-        __update()
-
-
-    def __gen_ID(self):
-    	RID = set()
-		with open("RID.txt","r") as file:
-		data = file.readlines()
-		for i in data : 
-			RID.append(i)
-
-		file.close()
-
-		x = uuid.uuid1()
-		while x in RID:
-			x = uuid.uuid1()
-
-		RID.add(x)
-
-		with open("RID.txt","w") as file:
-			for i in RID:
-				file.write(i+"\n")
-
-		file.close()
-
-		return x
-
-
-    def __update():
-        for i, k in enumerate(policy["health_records"].keys()):
-            encrypt = 0
-            for key, value in policy["health_records"][k].items():
-                if value==1:
-                    encrypt = 1
-                    break
-            if encrypt:
-                self.__shared_data.append(k)
-
-        public, private = self.__generateKeys()
-
-        self.public_key[-1] = public
-        self.private_key[-1] = 0
-
-        for ring in self.__shared_data:
-            public, private = self.__generateKeys()
-            self.public_key[ring] = public
-            self.private_key[ring] = private
-
-    def setPolicy(self, custom_policy ):
-        self.__policy = custom_policy
-        for d in (policy["health_records"]["Medical_Records_1"], policy["health_records"]["Medical_Records_2"], 
-            policy["health_records"]["Medical_Records_3"], policy["health_records"]["Addictions"]): self.__dataAccess.update(d)
-        __update()
-
-    def getPolicy(self):
-        return self.__policy
-
-    def getPublicKey(self):
-        return self.__publicKey
-
-    def __generateKeys(self):
-        private_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048,
-            backend=default_backend()
-        )
-        public_key = private_key.public_key()
-        return public_key, private_key
