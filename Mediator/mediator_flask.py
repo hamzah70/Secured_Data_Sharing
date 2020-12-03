3c42e5e59503b6a3346ff545c15591896bc2ab59
Anuneet Anand <anuneet18022@iiitd.ac.in>
2020-12-03 16:27:13 +0530

    Flask Errors

--- Mediator/mediator_flask.py
+++ Mediator/mediator_flask.py
@@ -17,7 +17,7 @@ def getInsurance():
 		ridNumber = result["rid"]
 		val = verifyRID(ridNumber)
 		if val==1:
-			r = requests.post('https://0.0.0.0:6000/', data = {"rid": ridNumber}, verify=False)
+			r = requests.post('https://0.0.0.0:5500/', data = {"rid": ridNumber}, verify=False)
 		else :
 			print("data not found")
 	return "abc"
@@ -28,10 +28,10 @@ def getHospital():
 	if request.method == 'POST':
 		result = request.form['result']
 		x = resolve(result)
-		requests.post('https://0.0.0.0:6000/get', data = {"x": x}, verify=False)
+		requests.post('https://0.0.0.0:5500/get', data = {"x": x}, verify=False)
 	return "Abc"
 
 
 if __name__ == "__main__":
 	# openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
-	app.run(host='0.0.0.0', port="6000", debug=True,ssl_context=('cert.pem', 'key.pem'))
+	app.run(host='0.0.0.0', port="5500", debug=True,ssl_context=('cert.pem', 'key.pem'))
